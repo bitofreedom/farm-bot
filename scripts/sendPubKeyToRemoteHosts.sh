a48bf35fa98e8b535a44f6796cafa63cd1f7bf14
sodakjack <darrindykstra@gmail.com>
2018-05-28 16:15:49 -0600

    another fix to script

--- scripts/sendPubKeyToRemoteHosts.sh
+++ scripts/sendPubKeyToRemoteHosts.sh
@@ -8,8 +8,9 @@ IFS=$'\n'       # make newlines the only separator
 set -f          # disable globbing
 for i in $(cat < "$1"); do
   echo "Sending .ssh/id_rsa.pub to Host: $i"
+  ssh root@$i 'mkdir -p .ssh'
   cat .ssh/id_rsa.pub | ssh root@$i 'cat >> .ssh/authorized_keys'
-  ssh root@$i "chmod 700 .ssh; chmod 640 .ssh/authorized_keys"
+  ssh root@$i 'chmod 700 .ssh; chmod 640 .ssh/authorized_keys'
 done
 
 echo "Script Ends..."
