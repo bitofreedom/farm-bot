3ad8b38f8e6215d08b4dcb50d3fff2f4f5ff5530
sodakjack <darrindykstra@gmail.com>
2018-06-26 16:57:05 -0600

    add emergency shutdown script

---
+++ scripts/emergencyShutdownAntminers.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+
+# Parameters:
+# IP List (file path)
+# remote host username
+
+IFS=$'\n'       # make newlines the only separator
+set -f          # disable globbing
+for i in $(cat < "$1"); do
+  echo "Rebooting Host: $i"
+  ssh $2@$i '/sbin/poweroff'
+done
+
+echo "Script Ends..."
